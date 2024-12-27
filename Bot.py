9a815bd6d11a8f299acf5125dc3982431d8911ae
Vismayp25205703 <Vismayp2520703@gmail.com>
2024-12-27 18:13:34 +0530

    Update

--- Bot.py
+++ Bot.py
@@ -81,7 +81,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.binary_location = "/home/render/chrome/opt/google/chrome/google-chrome"
+# chrome_options.binary_location = "/home/render/chrome/opt/google/chrome/google-chrome"
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
