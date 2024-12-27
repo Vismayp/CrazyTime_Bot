9913da7dbfebfbd405454664d085f15544809726
Vismayp25205703 <Vismayp2520703@gmail.com>
2024-12-27 12:38:40 +0530

    Start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
-apt-get update
-apt-get install -y wget
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
+#!/bin/bash
 
+# Update package list and install Chrome with sudo
+sudo apt-get update
+sudo apt-get install -y wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
 
 python3 Bot.py
