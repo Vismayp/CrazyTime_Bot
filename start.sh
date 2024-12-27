4c189eb9495c8d073d3c3edb4bfc35c44fb8f916
Vismayp25205703 <Vismayp2520703@gmail.com>
2024-12-27 12:35:30 +0530

    Start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
+apt-get update
+apt-get install -y wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+
 python3 Bot.py
